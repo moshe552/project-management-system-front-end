6ab08081563d1f504a5796f49e79ef9fc16a7788
moshe552 <mosdvo22@gmail.com>
2023-12-05 12:21:21 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import theme from "./theme.js";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Settings from "../src/pages/Settings/Setting.jsx";
 import ListProject from "./pages/Porjects/components/listProjects.jsx";
 import CreateProject from "./pages/create-project/components/createProject.jsx";
 
@@ -17,7 +16,7 @@ export default function App() {
           <main>
             <Routes>
               <Route path="/Projects/todo-board/:id" element={<TodoBoard />} />
-              <Route path="/Projects/todo-board/settings" element={<Settings />} />
+              {/* <Route path="/Projects/todo-board/settings" element={<Settings />} /> */}
               <Route path="/Projects" element={<ListProject/>} />
               <Route path="/Projects/creatProject" element={<CreateProject/>} />
             </Routes>
