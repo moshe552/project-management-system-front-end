5bad1a7674170e76eeeae346c8750d05fa78d3b8
aviv-taori <134504658+aviv-taori@users.noreply.github.com>
2023-12-19 11:41:37 +0200

    add user for server project

--- src/pages/Settings/DataAllUsers.js
+++ src/pages/Settings/DataAllUsers.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const myToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFAYS5jb20iLCJ0aW1lc3RhbXAiOjE3MDE1OTI1NjA5MjEsImlhdCI6MTcwMTU5MjU2MCwiZXhwIjo4NjU3MDE1OTI1NjB9.4o47zJ1r8215n0j8baBktRYqgMCMPlIBf-iSxRLkpII`
-const urlAllUsers = "http://localhost:5000/api/users/users"
+const urlAllUsers = "http://127.0.0.1:3000/users/all"
 const headers = {
     'Authorization': myToken, 
     'Content-Type': 'application/json; charset=utf-8'
@@ -9,5 +9,7 @@ const headers = {
 
   export default async function dataAllUsers () {
     const response = await axios.get( urlAllUsers ,{headers})
+    console.log(response.data.result)
+
     return response.data.result
 }
