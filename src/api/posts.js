01cfc222415392d1d5e71823f345f81530884081
michaelshoham <134501087+michaelshoham@users.noreply.github.com>
2023-12-31 17:39:41 +0200

    adding const token, api, haders

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,4 +1,12 @@
 const api = import.meta.env.VITE_SERVER_URL
+const token = localStorage.getItem("authToken");
 
-export {api} ;
+const { headers } = [
+    {
+        'Authorization': 'Happy',
+        'Content-Type': 'application/json; charset=utf-8',
+    }
+];
+
+export {api, token, headers} ;
 
